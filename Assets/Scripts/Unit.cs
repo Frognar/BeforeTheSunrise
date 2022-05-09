20c0b1a54497409a83c80b5f58d01db16d14537f
Frognar <sebastian.przyszlak@gmail.com>
2022-05-09 18:41:53 +0200

    Unit code clean-up

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
 using Pathfinding;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace bts {
   public class Unit : MonoBehaviour, Selectable {
@@ -10,6 +8,9 @@ namespace bts {
     public Affiliation ObjectAffiliation => Affiliation.Player;
     public Type ObjectType => Type.Unit;
     public GameObject Selected { get; private set; }
+    bool HasTarget => target != null && (target as Object) != null;
+    bool IsTimeToAttack => lastAttackTime + timeBetweenAttacks < Time.time;
+    bool IsOrderedToBuild => isOrderedToBuild && buildingToPlace != null;
 
     GridBuildingSystem gridBuildingSystem;
     PlacedObjectTypeSO buildingToPlace;
@@ -42,6 +43,10 @@ namespace bts {
       Selected.SetActive(false);
       isSelected = false;
       buildingToPlace = null;
+      ClearGhost();
+    }
+
+    void ClearGhost() {
       if (currentGhost != null) {
         Destroy(currentGhost.gameObject);
         currentGhost = null;
@@ -53,68 +58,59 @@ namespace bts {
     }
 
     void Update() {
+      HandleInput();
+
+      if (Vector3.Distance(transform.position, destination) < 2.5f) {
+        if (HasTarget && IsTimeToAttack) {
+          Attack();
+        }
+        else if (IsOrderedToBuild) {
+          Build();
+        }
+      }
+    }
+
+    void HandleInput() {
       if (isSelected && playerInputs.IsRightBtnDawn) {
         Ray ray = cam.ScreenPointToRay(playerInputs.MouseScreenPosition);
         if (Physics.Raycast(ray, out RaycastHit hitInfo)) {
-          if (hitInfo.transform.TryGetComponent(out Damageable damageable) && damageable.ObjectAffiliation != Affiliation.Player) {
-            target = damageable;
-            destination = hitInfo.transform.position;
-            MoveTo(destination);
-          }
-          else {
-            target = null;
-            if (buildingToPlace != null) {
-              destination = hitInfo.point;
-              isOrderedToBuild = true;
-              MoveTo(destination);
-              if (currentGhost != null) {
-                Destroy(currentGhost.gameObject);
-                currentGhost = null;
-              }
-            }
-            else {
-              MoveTo(hitInfo.point);
-            }
-          }
-        }
-      }
-
-      if (isOrderedToBuild && buildingToPlace != null) {
-        if (Vector3.Distance(transform.position, destination) < 2.5f) {
-          gridBuildingSystem.Build(destination, buildingToPlace);
-          buildingToPlace = null;
+          destination = hitInfo.point;
+          MoveTo(destination);
           isOrderedToBuild = false;
-        }
-      }
-
-      if (target != null && (target as Object) != null) {
-        if (Vector3.Distance(transform.position, destination) < 2.5f) {
-          if (lastAttackTime + timeBetweenAttacks < Time.time) {
-            target?.TakeDamage(5);
-            lastAttackTime = Time.time;
+          target = null;
+          ClearGhost();
+          if (buildingToPlace != null) {
+            isOrderedToBuild = true;
+          }
+          else if (hitInfo.transform.TryGetComponent(out Damageable damageable) && damageable.ObjectAffiliation != Affiliation.Player) {
+            target = damageable;
           }
         }
       }
     }
 
+    void Attack() {
+      target?.TakeDamage(5);
+      lastAttackTime = Time.time;
+    }
+
+    void Build() {
+      gridBuildingSystem.Build(destination, buildingToPlace);
+      buildingToPlace = null;
+      isOrderedToBuild = false;
+    }
+
     public void SetBuildingToBuild(PlacedObjectTypeSO buildingType) {
       if (isSelected) {
         buildingToPlace = buildingType;
-        if (currentGhost != null) {
-          Destroy(currentGhost.gameObject);
-          currentGhost = null;
-        }
-
+        ClearGhost();
         currentGhost = Instantiate(buildingType.ghost).GetComponent<GhostObject>();
       }
     }
 
     public void ClearBuildingToBuild() {
       buildingToPlace = null;
-      if (currentGhost != null) {
-        Destroy(currentGhost.gameObject);
-        currentGhost = null;
-      }
+      ClearGhost();
     }
   }
 }
