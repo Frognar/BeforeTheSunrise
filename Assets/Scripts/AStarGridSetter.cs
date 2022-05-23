d3b89d0e0881dc0fd8a7baa97890420d241e8ea9
Frognar <sebastian.przyszlak@gmail.com>
2022-05-23 20:51:37 +0200

    Adjust AStarGrid to map size.

---
+++ Assets/Scripts/AStarGridSetter.cs
@@ -0,0 +1,16 @@
+using Pathfinding;
+using UnityEngine;
+
+namespace bts {
+  public class AStarGridSetter : MonoBehaviour {
+    [SerializeField] IntAsset mapWidth;
+    [SerializeField] IntAsset mapHeight;
+    
+    void Awake() {
+      AstarData data = AstarPath.active.data;
+      GridGraph gg = data.graphs[0] as GridGraph;
+      gg.SetDimensions(mapWidth, mapHeight, 1f);
+      AstarPath.active.Scan();
+    }
+  }
+}
